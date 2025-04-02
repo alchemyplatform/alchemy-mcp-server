c824a3346d12eb5a7209b58b709c8f0716cbec00
Cody McCabe <codesmccabe@users.noreply.github.com>
2025-04-02 16:05:10 -0400

    feat: add prices api

---
+++ src/types/types.ts
@@ -0,0 +1,33 @@
+// NFT API
+export interface NFTOwnershipParams {
+  owner: string;
+  withMetadata?: boolean;
+  contractAddresses?: string[];
+  excludeFilters?: string[];
+  pageSize?: number;
+  pageKey?: string;
+  orderBy?: string;
+  tokenUriTimeoutInMs?: number;
+  spamConfidenceLevel?: string;
+}
+
+// Prices API
+export interface TokenPriceBySymbol {
+  symbols: string[];
+}
+
+export interface TokenPriceByAddress {
+  addresses: TokenPriceByAddressPair[];
+}
+
+export interface TokenPriceByAddressPair {
+  address: string;
+  network: string;
+}
+
+export interface TokenPriceHistoryBySymbol {
+  symbol: string;
+  startTime: string;
+  endTime: string;
+  interval: string;
+}
